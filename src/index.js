22abc09b7c3acc6e979270dc3b71fd95a5d974b6
Alfrheim <marc.badiac@gmail.com>
2019-04-05 23:14:33 +0200

    refactor creating display

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 const convert = require('./converter');
 import { clipboard } from 'electron';
-import React from 'react';
-import icon from './icon.png';
-import camelCaseIcon from './camelCase.png';
-import snakeCaseIcon from './snakeCase.png';
 
 const id = 'convert';
 export const fn = ({ term, actions, display, update }) => {
@@ -16,38 +12,16 @@ export const fn = ({ term, actions, display, update }) => {
         const onSelect = (event) => {
         };
 
-        display([
-            {
-            icon: camelCaseIcon,
-            title: `camelCase: ${convert.cases['camel'](textToConvert)}`,
-            onSelect: () => actions.copyToClipboard(convert.cases['camel'](textToConvert)),
-            getPreview: () => convert.cases['camel'](textToConvert)
-            },
-            {
-                icon: icon,
-                title: `PascalCase: ${convert.cases['pascal'](textToConvert)}`,
-                onSelect: () => actions.copyToClipboard(convert.cases['pascal'](textToConvert)),
-                getPreview: () => convert.cases['pascal'](textToConvert)
-            },
-            {
-                icon: icon,
-                title: `kebab-case: ${convert.cases['kebab'](textToConvert)}`,
-                onSelect: () => actions.copyToClipboard(convert.cases['kebab'](textToConvert)),
-                getPreview: () => convert.cases['kebab'](textToConvert)
-            },
-            {
-                icon: snakeCaseIcon,
-                title: `snake_case: ${convert.cases['snake'](textToConvert)}`,
-                onSelect: () => actions.copyToClipboard(convert.cases['snake'](textToConvert)),
-                getPreview: () => convert.cases['snake'](textToConvert)
-            },
-            {
-                icon: icon,
-                title: `space case: ${convert.cases['space'](textToConvert)}`,
-                onSelect: () => actions.copyToClipboard(convert.cases['space'](textToConvert)),
-                getPreview: () => convert.cases['space'](textToConvert)
-            }
-            ]
+
+        display(
+            Object.keys(convert.cases).map((key) => {
+                var convertedText = convert.cases[key](textToConvert);
+                return {
+                    title: `${convertedText}`,
+                    onSelect: () => actions.copyToClipboard(convertedText),
+                    getPreview: () => convertedText
+                };
+            })
         );
         console.log(clipboard.readText('selection'));
         let autocomplete = clipboard.readText('selection');
